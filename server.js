f4be56c8a296d20580643270ad32be3c2394da65
yash <yash@gmail.com>
2026-03-11 10:34:46 +0530

    add project files

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const crypto = require("crypto");
+
+const app = express();
+app.use(express.static("public"));
+
+const qrStore = new Map();
+
+app.get("/generate", (req, res) => {
+  const amount = req.query.amount;
+  const time = req.query.time;
+  const upi = req.query.upi;
+
+  const token = crypto.randomBytes(6).toString("hex");
+
+  qrStore.set(token, {
+    amount,
+    upi,
+    expiresAt: Date.now() + time * 60 * 1000,
+    used: false
+  });
+
+  res.send(`http://10.197.103.189:3000/scan/${token}`);
+});
+
+app.get("/scan/:token", (req, res) => {
+  const data = qrStore.get(req.params.token);
+
+  if (!data) return res.send("Invalid QR");
+  if (data.used) return res.send("QR Already Used");
+  if (Date.now() > data.expiresAt) return res.send("QR Expired");
+
+  data.used = true;
+
+  const upiUrl = `upi://pay?pa=${data.upi}&am=${data.amount}&cu=INR `;
+  res.redirect(upiUrl);
+});
+
+app.listen(3000,"0.0.0.0", () => {
+  console.log("Server running");
+});
