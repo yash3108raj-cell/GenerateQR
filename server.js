ffdecb6d1ffe4c59ae817b62b8373db378da22f4
yash3108raj-cell <yash3108raj@gmail.com>
2026-03-11 11:18:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,10 +34,28 @@ app.get("/scan/:token", (req, res) => {
 
   data.used = true;
 
-  const upiUrl = `upi://pay?pa=${data.upi}&am=${data.amount}&cu=INR `;
-  res.redirect(upiUrl);
-});
+  const upiUrl = `upi://pay?pa=${data.upi}&am=${data.amount}&cu=INR`;
 
+  res.send(`
+    <html>
+    <body style="font-family:sans-serif;text-align:center;margin-top:40px">
+      <h2>Pay with UPI</h2>
+
+      <a href="${upiUrl}" 
+         style="padding:15px 25px;background:#0f9d58;color:white;
+         text-decoration:none;border-radius:8px;font-size:18px">
+         Open UPI App
+      </a>
+
+      <script>
+        setTimeout(() => {
+          window.location.href = "${upiUrl}";
+        }, 500);
+      </script>
+    </body>
+    </html>
+  `);
+});
 app.listen(3000,"0.0.0.0", () => {
   console.log("Server running");
 });
